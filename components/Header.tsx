eec69aac4e36d105219315162a23f21c7a83e6b7
Rodrigo Martínez Vallejo <rodrigo.martinezvall@gmail.com>
2026-01-31 12:19:01 -0600

    delete space in header and made it sticky

--- components/Header.tsx
+++ components/Header.tsx
@@ -5,11 +5,11 @@ import SideNavegationBar from './SideNavegationBar';
 
 const Header = () => {
     return (
-        <header>
+        <header className='sticky top-0 bg-background/90 backdrop-blur-lg z-999'>
 
             <nav className='hidden sm:block'>
                 <ul className="flex justify-center items-center gap-8 p-5 border-b-2 border-merge">
-                    <li className='text-3xl ml-7 mr-auto font-extrabold uppercase'><Link href="/">Jimena Santillana</Link></li>
+                    <li className='text-3xl mr-auto font-extrabold uppercase'><Link href="/">Jimena Santillana</Link></li>
                     <li className='text-lg'><Link href="/about">Sobre mi</Link></li>
                     <li className='text-lg'><Link href="/events">Eventos</Link></li>
                     <li className='text-lg'><Link href="contact"><TextHighlight>Contacto</TextHighlight></Link></li>
